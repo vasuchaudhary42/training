37d6d0d40f338e386b608a16fd37050152a0bf0f
Vasu Chaudhary <vasu.chaudhary@gradeup.co>
2023-02-03 00:13:48 +0530

    registration

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
 
-router.get('/', authController.registration);
-
 router.post('/registration', authController.registration);
 
 module.exports = router;
